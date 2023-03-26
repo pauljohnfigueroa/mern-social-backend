f211e2c24b3d8680eaba046a4331ad3e4a6bdd75
Paul Figueroa <pjsfigueroa@gmail.com>
2023-03-26 20:09:20 +0800

    Add register user function in auth.js.

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,3 +2,42 @@ import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import User from '../models/User'
 
+const register = async (req, res) => {
+    try {
+        const {
+            firstName,
+            lastName,
+            email,
+            password,
+            picturePath,
+            friends,
+            location,
+            occupation,
+            viewedProfile,
+            impressions
+        } = req.body
+
+        const salt = await bcrypt.genSalt()
+        const passwordHash = await bcrypt.hash(password, salt)
+
+        const newUser = new User({
+            firstName,
+            lastName,
+            email,
+            password: passwordHash,
+            picturePath,
+            friends,
+            location,
+            occupation,
+            viewedProfile: Math.floor(Math.random() * 10000),
+            impressions: Math.floor(Math.random() * 10000)
+        })
+
+        const savedUser = newUser.save()
+
+        res.statuus(201).json(savedUser)
+
+    } catch (error) {
+        res.status(500).json({ errorMessage: error.message })
+    }
+}
