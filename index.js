0c07487f353d3a2dc6e9585203ea5f137f05ac4f
Paul Figueroa <pjsfigueroa@gmail.com>
2023-03-26 19:37:23 +0800

    Add User model.

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import morgan from 'morgan'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
+import { register } from './controllers/auth'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -37,6 +38,9 @@ const storage = multer.diskStorage({
     }
 })
 const upload = multer({ storage })
+
+/* Routes */
+app.post('/auth/register', upload.single('picture'), register)
 
 /* Database */
 const PORT = process.env.PORT || 6001
